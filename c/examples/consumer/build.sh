d7facfe16a87c0433086c9ae64fa51317cc5206e
BewareMyPower <xyzinfernity@163.com>
2019-09-10 17:25:31 +0800

    Use Makefile to build c/c++ examples

--- c/examples/consumer/build.sh
+++
@@ -1,1 +0,0 @@
-gcc -o qbus_consumer qbus_consumer_example.c -I../../src -L../../lib/debug -lQBus_c
--- c/examples/producer/build.sh
+++
@@ -1,1 +0,0 @@
-gcc -o qbus_producer qbus_producer_example.c -I../../src -L../../lib/debug -lQBus_c
