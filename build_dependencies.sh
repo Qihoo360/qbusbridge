01e74ead625635df39988447d554fa32086397a3
Yunze Xu <xyzinfernity@163.com>
2020-08-27 14:39:53 +0800

    Add boost 1.70 and protobuf 2.6.1 as 3rd party libraries

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -9,21 +9,54 @@ echo "const char* version = \"$VERSION\";" > ./cxx/src/kafka/util/version.cc
 git submodule init
 git submodule update
 
-INSTALL_DIR=$PWD/cxx/thirdparts/local
+SOURCE_DIR="$PWD/cxx/thirdparts"
+INSTALL_DIR="$SOURCE_DIR/local"
 
-# build librdkafka (support old gcc of CentOS 6)
-cd ./cxx/thirdparts/librdkafka
-./configure --prefix=$INSTALL_DIR --disable-ssl --disable-sasl
-make
-make install
+build_rdkafka() {
+    cd "$SOURCE_DIR/librdkafka"
+    ./configure --prefix="$INSTALL_DIR"
+    make
+    make install
+    cd -
+}
 
-cd -
+build_log4cplus() {
+    cd "$SOURCE_DIR/log4cplus"
+    ./scripts/fix-timestamps.sh
+    CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" ./configure --prefix="$INSTALL_DIR" --enable-static --with-pic
+    make
+    make install
+    cd -
+}
 
-# build log4cplus static library and support linked in a shared library
-cd ./cxx/thirdparts/log4cplus
-./scripts/fix-timestamps.sh
-CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" ./configure --prefix=$INSTALL_DIR --enable-static --with-pic
-make
-make install
+build_boost_1_70() {
+    pushd "$SOURCE_DIR"
+    if [[ ! -f boost_1_70_0.tar.gz ]]; then
+        wget https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz
+    fi
+    tar zxf boost_1_70_0.tar.gz
+    pushd boost_1_70_0
+    ./bootstrap.sh --prefix="$INSTALL_DIR" --with-libraries=regex,system
+    ./b2 cxxflags="-D_GLIBCXX_USE_CXX11_ABI=0 -fPIC" install
+    popd
+    popd
+}
 
-cd -
+build_protobuf_2_6() {
+    pushd "$SOURCE_DIR"
+    if [[ ! -f protobuf-2.6.1.tar.gz ]]; then
+        wget https://github.com/protocolbuffers/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
+    fi
+    tar zxf protobuf-2.6.1.tar.gz
+    pushd protobuf-2.6.1
+    CXX=g++ CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -fPIC" ./configure --prefix="$INSTALL_DIR"
+    make
+    make install
+    popd
+    popd
+}
+
+build_rdkafka
+build_log4cplus
+build_boost_1_70
+build_protobuf_2_6
